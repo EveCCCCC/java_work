7fe7239bc9e6c9b62600c0b31e1bc3f5492b64b9
Zheng Hong <zhongtan@uw.edu>
2016-09-04 11:36:41 +0800

    Added Chapter 2 exercises via upload.

---
+++ chapter2/BMICalculator.java
@@ -0,0 +1,18 @@
+public class BMICalculator {
+   public static void main(String[] args) {
+      // declare variables
+      double height;
+      double weight;
+      double bmi;
+      
+      // compute bmi 
+      height = 70;
+      weight = 195;
+      bmi = weight / (height * height) * 703;
+      
+      // print results 
+      System.out.println("Current BMI: ");
+      System.out.println(bmi);
+       
+   }
+}
